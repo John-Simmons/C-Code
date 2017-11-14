a03a3dbed135b537299810382efb25ae95cc0d47
jas124 <jas124@bucknell.edu>
2017-11-13 21:23:51 -0500

    FizzBuzz

---
+++ fizzbuzz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/*
+** A simple implementation of the class FizzBuzz
+*/
+int main(void){
+	
+	int i;
+	for(i = 0; i <= 100; i++){
+
+		if(i%3 == 0){
+			if(i%5 == 0){
+				printf("FizzBuzz\n");
+			}else{
+				printf("Fizz\n");
+			} 
+		}else if(i%5 == 0){
+			printf("Buzz\n");
+		}else{
+			printf("%d\n", i);
+		}	
+
+	}
+
+}
